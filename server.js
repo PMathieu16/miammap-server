17939a238562efa8fbbe4e7fe37fd828dafa9e1c
mathieu.parinet <mathieu.parinet@scub.net>
2022-12-07 11:26:55 +0100

    update server

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ import { Server } from "socket.io";
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
-  // options
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
   socket.on("join_room", (room, cb) => {
+    console.log("Connected");
     socket.join(room);
     socket.data.room = room;
     cb({
@@ -17,16 +21,25 @@ io.on("connection", (socket) => {
 
   /**
    * Sending to all clients in room except sender
-   * @param {string} action
+   * @param {string} name
    * @param {*} data
    */
-  const sender = (action, data) => {
-    socket.broadcast.to(socket.data.room).emit(action, data);
+  const sender = (name, data) => {
+    socket.broadcast.to(socket.data.room).emit(name, data);
   };
 
   socket.on("send_message", (data) => sender("get_message", data));
+
+  socket.on("send_arrivalPoint", (data) => sender("get_arrivalPoint", data));
+
+  socket.on("send_arrivalTime", (data) => send("get_arrivalTime", data));
+
+  socket.on("send_myData", (data) => {
+    console.log(socket.data.room, data);
+    sender("get_data", data);
+  });
 });
 
-httpServer.listen(3000, () => {
-  console.log(`Socket IO is running on port 3000.`);
+httpServer.listen(9999, () => {
+  console.log(`Socket IO is running on port 9999.`);
 });
